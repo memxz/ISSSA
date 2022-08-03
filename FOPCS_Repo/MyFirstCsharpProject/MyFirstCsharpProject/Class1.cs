f0d7ba9255c5bf9441448e7304099767002b4670
hardison <hardison@abc.com>
2022-08-03 15:47:56 +0800

    Add Day1 practise

---
+++ FOPCS_Repo/MyFirstCsharpProject/MyFirstCsharpProject/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCsharpProject
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, add a new class to my current project");
+            Console.ReadLine();
+        }
+    }
+}
