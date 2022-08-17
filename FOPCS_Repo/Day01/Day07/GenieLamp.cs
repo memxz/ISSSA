7acf5d441060bae29a6b81bb7739f868663241f7
hardison <hardison@abc.com>
2022-08-17 08:23:06 +0800

    Update day07

---
+++ FOPCS_Repo/Day01/Day07/GenieLamp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    class GenieLamp
+    {
+        public bool isOn;
+        private string color;
+        public string Color { get => color; set => color = value; }
+        public GenieLamp(string color)
+        {
+            isOn = false;
+            this.color = color;
+        }
+        public bool isLEDOn()
+        {
+            return isOn;
+        }
+        public void turnOn()
+        {
+            if (!isLEDOn())
+            {
+                isOn = true;
+            }
+        }
+        public void turnOff()
+        {
+            if (isLEDOn())
+            {
+                isOn = false;
+            }
+        }
+        public void lightOff()
+        {
+            turnOff();
+            if (this.Color == "red")
+            {
+                this.Color = "green";
+            }
+            else if (this.Color == "green")
+            {
+               this.Color = "blue";
+            }
+            else if (this.Color == "blue")
+            {
+                this.Color = "red";
+            }
+        }
+    }
+}
---
+++ FOPCS_Repo/Day01/Day07/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q1
+            GenieLamp led = new GenieLamp("red");
+            while (true)
+            {
+                Console.WriteLine("\n========================\n");
+                Console.WriteLine("1. Turn on the light \n2. Turn off the light. \n3. what is the color of the light \n4. Is the lamp ON? ");
+                Console.Write("Pls enter your selection: ");
+                int input = int.Parse(Console.ReadLine());
+                if (input == 1)
+                {
+                    led.turnOn();
+                }
+                else if (input == 2)
+                {
+                    if (led.isOn)
+                    {
+                        led.lightOff();
+                    }
+                    else
+                    {
+                        led.turnOff();
+                    }
+                }
+                else if (input == 3)
+                {
+                    Console.WriteLine(">>>" + led.Color + "<<<");
+                }
+                else if (input == 4)
+                {
+                    Console.WriteLine(">>>" + (led.isLEDOn() ? "ON" : "OFF") + "<<<");
+                }
+                else
+                {
+                    Console.WriteLine("Pls enter a valid number");
+                }
+            }
+
+
+
+        }
+    }
+}
