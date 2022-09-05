ab0ec90b2dc89c17b9ed030424bf6540f2691775
Cyrus <memsue@gmail.com>
2022-09-05 15:55:40 +0800

    update

---
+++ FOPCS_Repo/Day01/StackQueueImp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueImp
+{
+    internal class Program
+    {
+        public static bool checkBalanceDelimeters(string input) 
+        {
+            char[] arr = input.ToCharArray();
+            Stack<char> mystack = new Stack<char>();
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                if (arr[i] == '(')
+                    mystack.Push('(');
+                if (arr[i] == ')')
+                    mystack.Pop();
+                if (arr[i] == '[')
+                    mystack.Push('[');
+                if (arr[i] == ']')
+                    mystack.Pop();
+                if (arr[i] == '{')
+                    mystack.Push('{');
+                if (arr[i] == '}')
+                    mystack.Pop();
+
+
+            }
+            return mystack.Count ==0?true:false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(checkBalanceDelimeters("a {b [c (d + e)/2 - f  ] + 1}"));
+            Console.WriteLine(checkBalanceDelimeters("a {b [c (d + e)/2 - f  ] + 1"));
+            Console.Read();
+        }
+    }
+}
