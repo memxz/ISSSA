2c1a65243063c971647c9bcb92e5b363798c0e00
Cyrus <memsue@gmail.com>
2022-11-07 15:57:23 +0800

    add all

--- FOPCS_Repo/Day01/Day01/Program.cs
+++ FOPCS_Repo/Day01/Day01/Program.cs
@@ -10,8 +10,35 @@ namespace Day01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("John Smith\ne0011223@u.nus.edu");
-            Console.ReadLine();
+            //Console.WriteLine("John Smith\ne0011223@u.nus.edu");
+            for(; ; )
+            {
+                Console.Write("Pls enter interger: ");
+                string input=Console.ReadLine();
+                try
+                {
+                    int input2 = int.Parse(input);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Ouch! FormatException.");
+                    continue;
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("Ouch! OverflowException.");
+                    continue;
+                }
+                Console.WriteLine("Your number is " + input);
+                Console.ReadLine();
+                /*finally
+                {
+                    Console.WriteLine("Your number is " + input);
+                    Console.ReadLine();
+                }*/
+            }
+            
+           
         }
     }
 }
